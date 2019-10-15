6090a13c9fb8b2fdede7bcfc9e1803a31f620a48
Vanna Winland <vgwinland@gmail.com>
2019-10-15 18:25:51 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,10 +1,10 @@
 require "nokogiri"
-require 'pry'
-
+require "pry"
+ 
 def create_project_hash
-  html = File.read('fixutres/kickstarter.html')
+  html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
-
+ 
 create_project_hash
